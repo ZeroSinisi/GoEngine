dd8468fcbed98f97f77807ef5ca5863c1d5eade3
Jonathan Frahme <jmf6375+git@g.rit.edu>
2013-10-14 17:36:08 -0400

    Added checkValid

--- src/Board.java
+++ src/Board.java
@@ -29,12 +29,54 @@ public class Board {
 
 	// Attempt to place a piece. Return false if space is occupied
 	public boolean placePiece(int r, int c, Intersection.Piece p) {
-		if (board[r][c].getContents() == Intersection.Piece.EMPTY) {
+		if (checkValid(r, c, p) == true) {
 			setSpace(r, c, p);
 			checkRemoval(r, c);
 			return true;
 		} else
 			return false;
+	}
+
+	public boolean checkValid(int r, int c, Intersection.Piece p) {
+		boolean valid = false;
+		boolean checked[][] = new boolean[9][9];
+		Intersection tempBoard[][] = new Intersection[9][9];
+		copyBoard(tempBoard);
+		if (tempBoard[r][c].getContents() == Intersection.Piece.EMPTY) {
+			tempBoard[r][c].setContents(p);
+			checkRemovalGroup(r + 1, c, tempBoard, checked, p);
+			checkRemovalGroup(r - 1, c, tempBoard, checked, p);
+			checkRemovalGroup(r, c + 1, tempBoard, checked, p);
+			checkRemovalGroup(r, c - 1, tempBoard, checked, p);
+			List<Coord> group = new ArrayList<Coord>();
+			if (checkGroup(r, c, tempBoard[r][c].getContents(), checked, group,
+					0, tempBoard) != 0) {
+				valid = true;
+			}
+		}
+		return valid;
+	}
+	
+	private void copyBoard(Intersection[][] b){
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+				b[i][j]=new Intersection();
+				b[i][j].setContents(board[i][j].getContents());
+			}
+		}
+	}
+	private void checkRemovalGroup(int r, int c, Intersection[][] b,
+			boolean[][] checked, Intersection.Piece p) {
+		if (r >= 0 && r < 9 && c >= 0 && c < 9) {
+			if (b[r][c].getContents() != p
+					&& b[r][c].getContents() != Intersection.Piece.EMPTY
+					&& checked[r][c] == false) {
+				List<Coord> group = new ArrayList<Coord>();
+				if (checkGroup(r, c, b[r][c].getContents(), checked, group, 0, b) == 0) {
+					removeGroup(group, b);
+				}
+			}
+		}
 	}
 
 	public void checkRemoval() {
@@ -53,7 +95,7 @@ public class Board {
 		}
 	}
 
-	public void checkRemoval(int r, int c) {
+	/*public void checkRemoval(int r, int c) {
 		boolean checked[][] = new boolean[9][9];
 		for (int i = -1; i < 2; i++) {
 			for (int j = -1; j < 2; j++) {
@@ -71,11 +113,25 @@ public class Board {
 
 			}
 		}
+	}*/
+	
+	public void checkRemoval(int r, int c) {
+		boolean checked[][] = new boolean[9][9];
+		checkRemovalGroup(r+1,c,board,checked,board[r][c].getContents());
+		checkRemovalGroup(r-1,c,board,checked,board[r][c].getContents());
+		checkRemovalGroup(r,c+1,board,checked,board[r][c].getContents());
+		checkRemovalGroup(r,c-1,board,checked,board[r][c].getContents());
 	}
 
 	private void removeGroup(List<Coord> group) {
 		for (Coord groupMember : group) {
 			setSpace(groupMember.row, groupMember.col, Intersection.Piece.EMPTY);
+		}
+	}
+
+	private void removeGroup(List<Coord> group, Intersection[][] b) {
+		for (Coord groupMember : group) {
+			b[groupMember.row][groupMember.col].setContents(Intersection.Piece.EMPTY);
 		}
 	}
 
@@ -92,6 +148,26 @@ public class Board {
 				liberties += checkGroup(r, c - 1, p, checked, group, liberties);
 
 			} else if (board[r][c].getContents() == Intersection.Piece.EMPTY) {
+				return liberties + 1;
+			}
+		}
+		return liberties;
+	}
+
+	public int checkGroup(int r, int c, Intersection.Piece p,
+			boolean checked[][], List<Coord> group, int liberties,
+			Intersection b[][]) {
+		if (r >= 0 && c >= 0 && r < 9 && c < 9) {
+			if (b[r][c].getContents() == p && checked[r][c] == false) {
+				group.add(new Coord(r, c));
+				checked[r][c] = true;
+
+				liberties += checkGroup(r - 1, c, p, checked, group, liberties,b);
+				liberties += checkGroup(r, c + 1, p, checked, group, liberties,b);
+				liberties += checkGroup(r + 1, c, p, checked, group, liberties,b);
+				liberties += checkGroup(r, c - 1, p, checked, group, liberties,b);
+
+			} else if (b[r][c].getContents() == Intersection.Piece.EMPTY) {
 				return liberties + 1;
 			}
 		}
